1a73c9a67ce81eeefaf81930a46c702e067d7d57
n4kzu <79701697+n4kzu@users.noreply.github.com>
2021-04-01 10:53:04 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# admin-finder
-Una herramienta creada para buscar y encontrar el login/control-panel del Administrador.
+# Admin-Finder Tool
+**Con esta herramienta podrás encontrar el inicio de sesión del control-panel con un diccionario.**
+Este trae por defecto un diccionario con 134 posibles logins, aunque, si vos lo deseas, podrá cambiar el diccionario en el propio archivo.
+
+## Instalar y ejecutar
+Solo lo probé en mi máquina con Linux, pero supongo que no tendrá problema alguno al ejecutarlo en Windows.
+**Comandos a ejecutar:**
+```
+git clone https://github.com/n4kzu/admin-finder.git
+cd admin-finder/
+python3 admin-finder.py
+```
+Una vez instalado y ejecutado, solo falta completar las opciones que te pide.
+
+Además, deberás tener instaladas las librerías "requests" y "os"
+
+## Mis redes
+Twitter: [k3rbero_](https://twitter.com/k3rbero_) 
+Telegram: pr3tyx_.
---
+++ admin_finder.py
@@ -0,0 +1,86 @@
+#!/bin/python
+# Creado por: Pr3tyx/N4kzu/K3rbero
+
+import requests
+import os
+
+#Definir clase "color" con colores
+class color:
+    verde = '\033[92m'
+    naranja = '\033[93m'
+    rojo = '\033[91m'
+    end = '\033[0m'
+    negrita = '\033[1m'
+    azul = '\033[94m'
+
+#Definir escaneo
+def scan():
+    dic = "admin_pages"
+    file = open(dic, 'r')
+
+    for words in file:
+        words = words.strip()
+
+        r = requests.get(url+"/"+words)
+        r = r.status_code
+        r = str(r)
+        
+        if("200" in r):
+            print(f"{color.verde}[+] Posible Admin Page encontrado: "+url+"/"+words+f"{color.end}")
+            break
+        elif("403" in r):
+            print(f"{color.naranja}[~] Admin Page encontrado, pero bloqueado: "+url+"/"+words+f"{color.end}")
+            break
+        else:
+            print(f"{color.rojo}[-] Admin Page no encontrado: "+url+"/"+words+f"{color.end}")
+
+#Variable por defecto
+dic = "admin_pages"
+
+os.system("clear")
+
+banner = f'''{color.azul}
+-+- By: k3rbero/n4kzu/pr3tyx -+-
+
+ ▄▄▄▄    ▄▄▄        ██████   ██████ ▄▄▄█████▓ ██▓ ███▄    █   ▄████ 
+▓█████▄ ▒████▄    ▒██    ▒ ▒██    ▒ ▓  ██▒ ▓▒▓██▒ ██ ▀█   █  ██▒ ▀█▒
+▒██▒ ▄██▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒ ▓██░ ▒░▒██▒▓██  ▀█ ██▒▒██░▄▄▄░
+▒██░█▀  ░██▄▄▄▄██   ▒   ██▒  ▒   ██▒░ ▓██▓ ░ ░██░▓██▒  ▐▌██▒░▓█  ██▓
+░▓█  ▀█▓ ▓█   ▓██▒▒██████▒▒▒██████▒▒  ▒██▒ ░ ░██░▒██░   ▓██░░▒▓███▀▒
+░▒▓███▀▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░  ▒ ░░   ░▓  ░ ▒░   ▒ ▒  ░▒   ▒ 
+▒░▒   ░   ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░    ░     ▒ ░░ ░░   ░ ▒░  ░   ░ 
+ ░    ░   ░   ▒   ░  ░  ░  ░  ░  ░    ░       ▒ ░   ░   ░ ░ ░ ░   ░ 
+ ░            ░  ░      ░        ░            ░           ░       ░ 
+      ░
+                                          {color.negrita}{color.rojo}Admin Finder{color.end}
+'''
+print(banner+'\n')
+url = str(input(f"{color.negrita}{color.azul}[*] Ingresa la página{color.end} (ej: http://ejemplo.com): "))
+if("https://" in url):
+    pass
+elif("http://" in url):
+    pass
+else:
+    print(f"{color.naranja}{color.negrita}[~] No se ha detectado 'https' o 'http'.")
+    url1 = input(f"[*]1: HTTP\n[*]2: HTTPS\n[~]¿Cual desea usar?: ")
+    if(url1 =="1"):
+        url1 = "http://"
+        url = url1+url
+    elif(url1 =="2"):
+        url1 = "https://"
+        url = url1+url
+    else:
+        print(f"{color.red}[-] Error. Reinicia el programa.{color.end}")
+        exit    
+print(f"{color.azul}{color.negrita}[*] Página especificada: {color.end}"+url)
+a = str(input(f"{color.negrita}{color.azul}[*] Ruta del diccionario {color.end}(Enter por defecto): {color.end}"))
+if(a != ''):
+    dic = a
+else:
+    pass
+
+print(f"{color.negrita}{color.azul}[*] Diccionario actual: {color.end}" + dic)
+print(f"{color.negrita}{color.azul} -+- -+- -+- ### -+- -+- -+- {color.end}")
+print(f"{color.negrita}{color.azul} [~] Cargando...{color.end}")
+print(f"{color.negrita}{color.azul} -+- -+- -+- ### -+- -+- -+- {color.end}")
+scan()
